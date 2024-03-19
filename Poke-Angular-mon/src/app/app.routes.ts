bd03a9ab9b0f74b9cc7f1ac73fa8633a93a59368
Thomaz Rosseti <tocorosseti@gmail.com>
2024-03-19 15:45:40 -0300

    feat: initial route configuration

--- Poke-Angular-mon/src/app/app.routes.ts
+++ Poke-Angular-mon/src/app/app.routes.ts
@@ -1,3 +1,11 @@
+import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [];
+
+@NgModule({
+  imports: [],
+  exports: [],
+})
+
+export class AppRoutingModule {}
