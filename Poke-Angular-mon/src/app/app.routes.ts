d8da80eec8864a1829b54853532ce2781ad1665b
Thomaz Rosseti <tocorosseti@gmail.com>
2024-03-25 22:30:42 -0300

    feat: home page created

--- Poke-Angular-mon/src/app/app.routes.ts
+++ Poke-Angular-mon/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
 export const routes: Routes = [
-  {path: '/about', component: AboutComponent}
+  {path: '/about', component: AboutComponent},
+  {path: '/', component: HomeComponent}
 ];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
