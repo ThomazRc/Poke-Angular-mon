1b6e2e0bad644aee648b109115cc29b7c550a2fd
Thomaz Rosseti <tocorosseti@gmail.com>
2024-03-27 21:36:09 -0300

    feat: error page created

--- Poke-Angular-mon/src/app/app.routes.ts
+++ Poke-Angular-mon/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { ErrorComponent } from './components/pages/error/error.component';
 
 export const routes: Routes = [
   {path: '/about', component: AboutComponent},
-  {path: '/', component: HomeComponent}
+  {path: '/', component: HomeComponent},
+  {path: '*', component: ErrorComponent },
 ];
 
 
